cdf263c5ce69d1e7e201419c6559e2a6ee52d706
anik <anikdebb.ee21@rvce.edu.in>
2022-11-30 12:33:49 +0530

    added layout management and created title

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -2,26 +2,22 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
-import Navs from './components/Navs';
 
 function App() {
   return (
-    <div>
-      <Navs />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
 
-        <Route>
-          <div>not found</div>
-        </Route>
-      </Switch>
-    </div>
+      <Route>
+        <div>not found</div>
+      </Route>
+    </Switch>
   );
 }
 
